2af784087e65290ebe68e4cab1a71b647cf6742f
roypunga <royseba2005@gmail.com>
2024-09-06 16:58:06 -0300

    Create createDivs() and eraseBoard() functions

---
+++ script.js
@@ -0,0 +1,33 @@
+// random color = 'rgb(' + Math.random() * 255 + ', ' + Math.random() * 255 + ', ' + Math.random() * 255 + ')'
+
+
+const grid = document.querySelector('.grid');
+
+function getGridElements(){
+    const divsInGrid = document.querySelectorAll('.grid div');
+    return divsInGrid;
+}
+
+function createDivs(input){
+
+    for(let i = 0; i < input * input; i++){
+        const newDiv = document.createElement('div');
+        newDiv.style.height = 'auto';
+        newDiv.style.width = 'auto';
+        newDiv.style.minWidth = 400 / input + 'px';
+        newDiv.style.minHeight = 400 / input + 'px';
+        newDiv.style.flexGrow = '1';
+
+        newDiv.style.backgroundColor = 'rgb(' + Math.random() * 255 + ', ' + Math.random() * 255 + ', ' + Math.random() * 255 + ')';
+
+        grid.appendChild(newDiv);
+    }
+
+}
+
+function eraseBoard(){
+
+    getGridElements().forEach((element) => element.remove());
+}
+
+// function applySettings();
