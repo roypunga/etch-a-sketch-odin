7f7f419b650157edc66afbad325e9e2fb08cd0a0
roypunga <royseba2005@gmail.com>
2024-09-07 00:47:20 -0300

    Get integrate applySettings() into createDivs(), add checkboxes logic and link functions with inputs

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 
 
 const grid = document.querySelector('.grid');
-const rainbow = document.querySelector('#rainbow')
+const rainbow = document.querySelector('#rainbow');
+const colors = document.querySelector('.colors');
+const eraser = document.querySelector('#eraser');
+const slider = document.querySelector('.gridSize');
+const resetButton = document.querySelector('.reset')
 
 function getGridElements(){
     const divsInGrid = document.querySelectorAll('.grid div');
@@ -20,8 +24,18 @@ function createDivs(input){
         newDiv.style.minWidth = 400 / input + 'px';
         newDiv.style.minHeight = 400 / input + 'px';
         newDiv.style.flexGrow = '1';
+        newDiv.addEventListener('mouseover', () =>{
+            if(rainbow.checked == true){
+                newDiv.style.backgroundColor = 'rgb(' + Math.random() * 255 + ', ' + Math.random() * 255 + ', ' + Math.random() * 255 + ')'
+            }
+            else if(eraser.checked == true){
+                newDiv.style.backgroundColor = 'white';
+            }
+            else {
+                newDiv.style.backgroundColor = document.querySelector('.colorPick').value;
+            }
+        })
 
-//        newDiv.style.backgroundColor = 'rgb(' + Math.random() * 255 + ', ' + Math.random() * 255 + ', ' + Math.random() * 255 + ')';
 
         grid.appendChild(newDiv);
     }
@@ -32,12 +46,33 @@ function eraseBoard(){
     getGridElements().forEach((element) => element.remove());
 }
 
-function applySettings(){
-    getGridElements().forEach((element) => {
-        element.addEventListener('mouseover', () =>{
-            if(rainbow.checked == true){
-                element.style.backgroundColor = 'rgb(' + Math.random() * 255 + ', ' + Math.random() * 255 + ', ' + Math.random() * 255 + ')'
-            }
-        })
-    })
+function checkboxesLogic(){
+
+    if(rainbow.checked == true){
+        colors.style.display = 'none';
+        eraser.checked = false;
+    }
+    if(eraser.checked == true){
+        colors.style.display = 'none';
+        rainbow.checked = false;
+    }
+    if(eraser.checked == false && rainbow.checked == false) {
+        colors.style.display = 'block';
+    }
 }
+
+rainbow.addEventListener('click', ()=> {
+    checkboxesLogic();
+});
+eraser.addEventListener('click', ()=> {
+    checkboxesLogic();
+});
+slider.addEventListener('click', ()=> {
+    eraseBoard();``
+    createDivs(slider.value);
+})
+resetButton.addEventListener('click', ()=> {
+    getGridElements().forEach((element) => element.style.backgroundColor = 'white')
+})
+
+createDivs(50);
